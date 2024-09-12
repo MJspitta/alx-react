64e9870cb574b63da033077011c7581c3cffcfbf
Madu Jang <madu.jang93@gmail.com>
2024-09-13 00:41:40 +0100

    pure component

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Notifications.css';
+import PropTypes from 'prop-types';
+
+
+class NotificationItem extends React.PureComponent {
+    render() {
+        const { type, value, html, markAsRead, id } = this.props;
+
+        return (
+            <div>
+                {type && value ? <li data-notification-type={type} onClick={() => markAsRead(id)}>{value}</li> : null}
+                {html ? <li onClick={() => markAsRead(id)} data-urgent="true" dangerouslySetInnerHTML={{ __html: html }}></li> : null}
+            </div>
+        );
+    }
+}
+
+NotificationItem.propTypes = {
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    __html: PropTypes.shape({
+        html: PropTypes.string,
+    }),
+    markAsRead: PropTypes.func,
+    id: PropTypes.number,
+};
+
+NotificationItem.defaultProps = {
+    type: "default",
+    markAsRead: () => {
+        console.log("empty function");
+    },
+    id: 0,
+}
+
+export default NotificationItem;

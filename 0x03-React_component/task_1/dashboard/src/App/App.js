ad2ee70c6b76de8eba03009cb31e08ad280bc3ee
Madu Jang <madu.jang93@gmail.com>
2024-09-12 18:38:29 +0100

    fix object

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
   listNotifications = [
     { id: 1, type: 'default', value: 'New course available' },
     { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: getLatestNotification() },
+    { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
   ];
 
   handleKeyDown(e) {

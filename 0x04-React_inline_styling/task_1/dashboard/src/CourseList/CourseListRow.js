dbf9d4dd6531e56e52e4fd38821c2a1b9e51bada
Madu Jang <madu.jang93@gmail.com>
2024-09-18 23:25:22 +0100

    aphrodite inline styling

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './CourseList.css';
+
+const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+
+    const styleRow = isHeader ? { backgroundColor: '#deb5b545' } : { backgroundColor: '#f5f5f5ab' };
+
+    return (
+        <tr style={styleRow}>
+            {isHeader ? (
+                textSecondCell === null ? (
+                    <th colSpan={2}>{textFirstCell}</th>
+                ) : (
+                    <>
+                        <th className='al-left'>{textFirstCell}</th>
+                        <th className='al-left'>{textSecondCell}</th>
+                    </>
+                )
+            ) : (
+                <>
+                    <td>{textFirstCell}</td>
+                    <td>{textSecondCell}</td>
+                </>
+            )}
+        </tr>
+    );
+};
+
+CourseListRow.propTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+CourseListRow.defaultProps = {
+    isHeader: false,
+    textSecondCell: null,
+};
+
+export default CourseListRow;

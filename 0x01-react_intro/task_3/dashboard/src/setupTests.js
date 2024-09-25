8814ee77cc7fe5dc8ce991c1b38c10aa64e41b1a
Madu Jang <madu.jang93@gmail.com>
2024-09-25 21:34:04 +0100

    fix

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,10 @@
+global.TextEncoder = global.TextEncoder || require('util').TextEncoder;
+global.TextDecoder = global.TextDecoder || require('util').TextDecoder;
+global.fetch = require('node-fetch');
+
+
 import Enzyme from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
+
 
 Enzyme.configure({ adapter: new Adapter() });

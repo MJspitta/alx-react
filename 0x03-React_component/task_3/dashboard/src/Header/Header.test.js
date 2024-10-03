e4a0644fac7f26ac5fb36c720ef9551ebb640a27
Madu Jang <madu.jang93@gmail.com>
2024-10-03 03:34:02 +0100

    BodySection component and tests

--- 0x03-React_component/task_3/dashboard/src/Header/Header.test.js
+++ 0x03-React_component/task_3/dashboard/src/Header/Header.test.js
@@ -2,14 +2,17 @@ import React from "react";
 import Header from "./Header";
 import { shallow } from "enzyme";
 
-describe("Header", () => {
-  it("render without crashing", () => {
+
+describe("Header component", () => {
+  it("renders without crashing", () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.exists()).toEqual(true);
+    expect(wrapper.exists()).toBe(true);
   });
-  it("should render a h1", () => {
+
+  it("render h1 and img tags", () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.exists("img")).toEqual(true);
-    expect(wrapper.containsMatchingElement(<h1>School dashboard</h1>)).toEqual(true);
+    expect(wrapper.exists("img")).toBe(true);
+    expect(wrapper.containsMatchingElement(<h1>School dashboard</h1>)).toBe(true);
   });
+
 });

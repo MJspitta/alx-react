c64282ed099f06d655151eacf0268d1f56afc670
Madu Jang <madu.jang93@gmail.com>
2024-12-11 15:58:25 +0100

    connect action creators

---
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,27 @@
+import { Map } from 'immutable';
+import * as actions from '../actions/uiActionTypes';
+
+const initialState = Map({
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {},
+});
+
+const uiReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actions.DISPLAY_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', true);
+    case actions.HIDE_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', false);
+    case actions.LOGIN_SUCCESS:
+      return state.set('isUserLoggedIn', true)
+    case actions.LOGIN_FAILURE:
+      return state.set('isUserLoggedIn', false)
+    case actions.LOGOUT:
+      return initialState;
+    default:
+      return state; // Return current state for unhandled actions
+  }
+};
+
+export default uiReducer;
